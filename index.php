6f0a3353d61ccd71d9da4219065c0d905564b3cf
Zelidee <zelideeguemez@gmail.com>
2025-04-03 18:59:41 -0400

    Implementacion de funcion para leer personajes

--- index.php
+++ index.php
@@ -23,9 +23,27 @@ Template::applyTemplate();
      <tbody>
           <?php
           // lista de personajes
+          
 
           // Boton para editar
           ?>
           <a href="register.php?id=" <?= $character->id ?>" class="button">Editar</a>
      </tbody>
 </table>
+
+
+
+<?php
+function listCharacters()
+{
+     $characters = [];
+     foreach (scandir(DATA_FOLDER) as $file) {
+
+          $filePath = DATA_FOLDER . $file;
+          if (!is_file($filePath)) continue;
+
+          $id = pathinfo($file, PATHINFO_FILENAME); //obtiene solo el nombre sin ".json"
+          $characters[] = getCharacter($id);
+     }
+     return $characters;
+}
