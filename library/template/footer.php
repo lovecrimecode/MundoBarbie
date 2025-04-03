dadb827b0b4885ae7ec42b77e4cc5a68e5afbc27
Zelidee <zelideeguemez@gmail.com>
2025-04-03 19:34:14 -0400

    Pase de funciones a compnente.php, implementacion completa de la vista de personajes, arreglos menores

--- library/template/footer.php
+++ library/template/footer.php
@@ -1,4 +1,3 @@
-?>
 <hr>
 <h4 class="alt-text-border">Hecho por: Zelidee Guemez<br>Matricula: 2023-1706</h4>
 </div>
--- register.php
+++ register.php
@@ -1,5 +1,6 @@
 <?php
 require 'library/template/template.php';
+require 'library/components.php';
 Template::applyTemplate();
 
 $character = new Character();
@@ -86,28 +87,3 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
         }
     });
 </script>
-
-<?php
-// Definición de el folder "data/" en donde iran los personajes como "DATA_FOLDER"
-define("DATA_FOLDER", "data/");
-
-// Función para guardar un personaje en el folder de datos
-function storeCharacter($id, $data)
-{
-    if (!is_dir(DATA_FOLDER)) {
-        mkdir(DATA_FOLDER, 0777, true);
-    }
-
-    $file = DATA_FOLDER . "{$id}.json"; // Archivo json con los datos del personaje segun id
-    return file_put_contents($file, json_encode($data, JSON_PRETTY_PRINT)) !== false;
-}
-
-function updateCharacter($id, $data)
-{
-    $file = DATA_FOLDER . "{$id}.json";
-    if (file_exists($file)) {
-        return file_put_contents($file, json_encode($data, JSON_PRETTY_PRINT)) !== false;
-    } else {
-        return false;
-    }
-}
